c80bf9940dcd16d497ce85fc1e13dd604ada7e61
Furo <rikygerma9@gmail.com>
2023-05-26 18:50:24 +0200

    Prova commit

--- utils.py
+++ utils.py
@@ -10,3 +10,4 @@ def blit_rotate_center(win, image, top_left, angle):
     new_rect = rotated_image.get_rect(
         center=image.get_rect(topleft=top_left).center)
     win.blit(rotated_image, new_rect.topleft)
+    
